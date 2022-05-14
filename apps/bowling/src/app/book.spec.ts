16500982c2a8b0a2c6c3883993d30e639e43a7be
unknown <eliliu9923.dif06@nctu.edu.tw>
2022-05-14 23:30:02 +0800

    Simplecase: Several Discounts(G)

--- apps/bowling/src/app/book.spec.ts
+++ apps/bowling/src/app/book.spec.ts
@@ -1,3 +1,4 @@
+// book.spec.ts
 import { Book } from './book';
 
 describe('Book', () => {
@@ -6,11 +7,80 @@ describe('Book', () => {
   });
 });
 
-// book.spec.ts
-test('Buy one book', () => {
+test('Buy no book', () => {
   const books = new Book();
-  let buying: number[] = [1, 2, 2];
+  let buying: number[] = [];
+  books.bnum(buying);
+  expect(books.price).toBe(0);
+});
+
+test('Buy one book(ep 1)', () => {
+  const books = new Book();
+  let buying: number[] = [1];
+  books.bnum(buying);
+  expect(books.price).toBe(100);
+});
+
+test('Buy two books(ep 1, 2)', () => {
+  const books = new Book();
+  let buying: number[] = [1, 2];
     books.bnum(buying);
   
+  expect(books.price).toBe(190);
+});
+
+test('Buy three books(ep 1, 1, 1)', () => {
+  const books = new Book();
+  let buying: number[] = [1, 1, 1];
+  books.bnum(buying);
+  expect(books.price).toBe(300);
+});
+
+test('Buy three books(ep 1, 2, 3)', () => {
+  const books = new Book();
+  let buying: number[] = [1, 2, 3];
+  books.bnum(buying);
+  expect(books.price).toBe(270);
+});
+
+test('Buy three books(ep 1, 2, 2)', () => {
+  const books = new Book();
+  let buying: number[] = [1, 2, 2];
+  books.bnum(buying);
   expect(books.price).toBe(290);
+});
+
+test('Buy four books(ep 1, 2, 3, 5)', () => {
+  const books = new Book();
+  let buying: number[] = [1, 2, 3, 5];
+  books.bnum(buying);
+  expect(books.price).toBe(320);
+});
+
+test('Buy four books(ep 1, 1, 2, 2)', () => {
+  const books = new Book();
+  let buying: number[] = [1, 1, 2, 2];
+  books.bnum(buying);
+  expect(books.price).toBe(380);
+});
+
+test('Buy five books(ep 1, 2, 3, 4, 5)', () => {
+  const books = new Book();
+  let buying: number[] = [1, 2, 3, 4, 5];
+  books.bnum(buying);
+  expect(books.price).toBe(375);
+});
+
+test('Buy six books(ep 1, 1, 2, 3, 3, 4)', () => {
+  const books = new Book();
+  let buying: number[] = [1, 1, 2, 3, 3, 4];
+  books.bnum(buying);
+  expect(books.price).toBe(510);
+});
+
+test('Buy six books(ep 1, 2, 2, 3, 4, 5)', () => {
+  const books = new Book();
+  let buying: number[] = [1, 2, 2, 3, 4, 5];
+  books.bnum(buying);
+  expect(books.price).toBe(475);
 });
--- apps/bowling/src/app/book.ts
+++ apps/bowling/src/app/book.ts
@@ -11,10 +11,6 @@ export class Book {
             this.bookcount[bookname-1] += 1;
         }
         this._price = 100*this.booktotal
-
-        // if (this.booktotal = 2){
-        //     this._price = this._price*(1-discount[1])
-        // }
         this.bcom();
 
     }
@@ -22,44 +18,30 @@ export class Book {
     bcom(){
         
         let discount = [0, 0.05, 0.1, 0.2, 0.25]
-        const someIsNotZero = this.bookcount.some(item => item !== 0);
         var indices: number[] = [];
         var notzero: number[] = [];
-        //var notzero: number[] = [];
         var orin = [0, 1, 2, 3, 4]
         var booktest = this.bookcount;
-        //var notzero = 0
         var idx = this.bookcount.indexOf(0);
 
         while (idx != -1) {
         indices.push(idx);
         idx = booktest.indexOf(0, idx + 1);
         }
-        console.log(indices); // [0, 2, 4]
+        // console.log(indices); // [0, 2, 4]
 
         notzero = orin.filter( function( el ) {
             return !indices.includes( el );
           } );
-        console.log(notzero);
+        // console.log(notzero);
         
     
         //notzero = 5-indices.length
 
         while(notzero.length > 1){
-            if (notzero.length == 2){
-                this._price = this._price - 100*2*(discount[1]);
-                for (let eachnot of notzero) {
-                    booktest[eachnot] -=1;
-                }
-            }
-            else if (notzero.length == 3){
-
-            }
-            else if (notzero.length == 4){
-
-            }
-            else if (notzero.length == 5){
-
+            this._price = this._price - 100*notzero.length*(discount[notzero.length-1]);
+            for (let eachnot of notzero) {
+                booktest[eachnot] -=1;
             }
             indices = []
             idx = booktest.indexOf(0);
@@ -74,12 +56,6 @@ export class Book {
     }
 
     get price() {
-        //console.log(this.bookcount);
-        //console.log(this.booktotal);
-
         return this._price;
       }
 }
-// def assert_equal{
-//     (8 * 2 * 0.95, price([0, 1]))
-// }
