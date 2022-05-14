1badd5c9417843040e767987ecd64cefe0f5300c
unknown <eliliu9923.dif06@nctu.edu.tw>
2022-05-14 20:42:30 +0800

    First Case: BuyOneBook(G)

--- apps/bowling/src/app/book.ts
+++ apps/bowling/src/app/book.ts
@@ -1,5 +1,20 @@
+// book.ts
 export class Book {
+    bnum(pins: number) {
+        //bookcount = 1
+    }
+
+    get price() {
+
+        return 100;
+      }
 }
 // def assert_equal{
 //     (8 * 2 * 0.95, price([0, 1]))
 // }
+
+export class Game {
+    roll(pins: number) {}
+  
+    
+  }
